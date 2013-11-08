99118aa0ffaaa9a9f0378cee8a7dfe944aacd27f
Patrick Turley [pturley] <patrick.turley@gmail.com>
2013-11-08 12:20:36 -0600

    initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ULSafety::Application.config.secret_key_base = 'd6e2e0642ec5ecc27d3c6a235659ae8727cc90b47b50a426573e76804b80a8ada30276b24848037cad242f9f14362273e743327952e7ac39525112856f325c3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ULSafety::Application.config.session_store :cookie_store, key: '_ULSafety_session'
