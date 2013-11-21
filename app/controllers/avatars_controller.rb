c1b3e1219da010a28c1d0f9964577fd223ade02d
Stacey Johnson <sjohnson@icstars.org>
2013-11-20 19:54:41 -0600

    avatars

---
+++ app/controllers/avatars_controller.rb
@@ -0,0 +1,6 @@
+class AvatarsController < ApplicationController
+	def index
+end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ ULSafety::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+    get 'avatars'=>'avatars#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
